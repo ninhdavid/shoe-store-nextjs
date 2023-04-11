56b707d92d12ce64f6a519d9521ced93bd9658aa
ninh <ninhdavid96@gmail.com>
2023-04-11 21:37:01 +0700

    FE: add api. Be: setup Strapi, cloud

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,31 +3,40 @@ import { Inter } from 'next/font/google';
 import HeroBanner from '@/components/HeroBanner';
 import Wrapper from '@/components/Wrapper';
 import ProductCard from '@/components/ProductCard';
+import { useEffect, useState } from 'react';
+import { fetchDataFromApi } from '@/utils/api';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home({ products }) {
+	// const [data, setData] = useState(null);
+	// useEffect(() => {
+	// 	fetchProduct();
+	// }, []);
+	// const fetchProduct = async () => {
+	// 	const { data } = await fetchDataFromApi('/api/products');
+	// 	setData(data);
+	// };
 	return (
 		<main>
 			<HeroBanner />
 			<Wrapper>
 				{/* products grid start */}
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-					{/* {products?.data?.map((product) => (
-                        <ProductCard key={product?.id} data={product} />
-                    ))} */}
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
-					<ProductCard />
+					{products?.data?.map((product) => (
+						<ProductCard key={product?.id} data={product} />
+					))}
 				</div>
 				{/* products grid end */}
 			</Wrapper>
 		</main>
 	);
+}
+
+export async function getStaticProps() {
+	const products = await fetchDataFromApi('/api/products?populate=*');
+
+	return {
+		props: { products },
+	};
 }
